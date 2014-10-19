474ebc26278ef66a76933fd9d618d2e7d651d6e0
Konstantin Stepanov <me@kstep.me>
2014-10-19 13:48:00 +0300

    development

--- src/api.rs
+++ src/api.rs
@@ -7,7 +7,7 @@ use url::Url;
 use std::io;
 use std::io::{standard_error, IoResult, IoError};
 use std::str::from_utf8;
-use objects::{Envelope, Cursor, Timestamp, Error, PbObj, Iden};
+use objects::{Envelope, Cursor, Timestamp, Error, PbObj, Iden, Push};
 use messages::PbMsg;
 use serialize::{Encodable, Decodable, Encoder, Decoder};
 
@@ -23,9 +23,6 @@ pub struct PbAPI {
     api_key: String,
 }
 
-pub trait JsonEncodable<'a> : Encodable<json::Encoder<'a>, IoError> {}
-pub trait JsonDecodable : Decodable<json::Decoder, json::DecoderError> {}
-
 impl PbAPI {
     fn make_writer(&self, method: Method, url: &str) -> IoResult<RequestWriter> {
         let mut writer = try!(RequestWriter::new(method, match Url::parse(url) {
@@ -38,6 +35,21 @@ impl PbAPI {
 
     pub fn new(api_key: &str) -> PbAPI {
         PbAPI{ api_key: api_key.to_string() }
+    }
+
+    fn get(&self, path: &str, params: &[(&str, &str)]) -> IoResult<String> {
+        let url = format!("{}{}?{}", BASE_URL, path, params.iter().filter(|v| *v.ref1() != "").map(|&(k, v)| format!("{}={}&", k, v)).fold("".to_string(), |acc, item| acc + item));
+        let writer = try!(self.make_writer(Get, url.as_slice()));
+
+        match writer.read_response() {
+            Ok(ref mut resp) => {
+                match from_utf8(try!(resp.read_to_end()).as_slice()) {
+                    Some(ref r) => Ok(r.to_string()),
+                    _ => return Err(standard_error(io::InvalidInput))
+                }
+            },
+            Err((_, err)) => Err(err)
+        }
     }
 
     fn post(&self, path: &str, content: &str) -> IoResult<String> {
@@ -57,10 +69,6 @@ impl PbAPI {
         }
     }
 
-    pub fn save<'a, T: PbMsg + JsonEncodable<'a>, R: PbObj + JsonDecodable>(&self, obj: &str, msg: &T) -> IoResult<R> {
-        self.post(obj, json::encode(msg).as_slice()).map(|v| json::decode(v.as_slice()).unwrap())
-    }
-
     fn delete(&self, path: &str) -> IoResult<()> {
         let url = format!("{}{}", BASE_URL, path);
         let writer = try!(self.make_writer(Delete, url.as_slice()));
@@ -71,30 +79,20 @@ impl PbAPI {
         }
     }
 
-    pub fn remove(&self, obj: &str, iden: Iden) -> IoResult<()> {
-        self.delete(format!("{}/{}", obj, iden).as_slice())
+    pub fn save<'a, T: PbMsg, R: PbObj>(&self, obj: &str, msg: &T) -> IoResult<R>
+        where T: Encodable<json::Encoder<'a>, IoError>, R: Decodable<json::Decoder, json::DecoderError> {
+        self.post(obj, json::encode(msg).as_slice()).map(|v| json::decode(v.as_slice()).unwrap())
     }
 
-    fn get(&self, path: &str, params: Vec<(&str, &str)>) -> IoResult<String> {
-        let url = format!("{}{}?{}", BASE_URL, path, params.iter().filter(|v| *v.ref1() != "").map(|&(k, v)| format!("{}={}&", k, v)).fold("".to_string(), |acc, item| acc + item));
-        let writer = try!(self.make_writer(Get, url.as_slice()));
-
-        match writer.read_response() {
-            Ok(ref mut resp) => {
-                match from_utf8(try!(resp.read_to_end()).as_slice()) {
-                    Some(ref r) => Ok(r.to_string()),
-                    _ => return Err(standard_error(io::InvalidInput))
-                }
-            },
-            Err((_, err)) => Err(err)
-        }
+    pub fn remove(&self, obj: &str, iden: Iden) -> IoResult<()> {
+        self.delete(format!("{}/{}", obj, iden).as_slice())
     }
 
     #[inline] fn _load(&self, obj: &str, limit: Option<uint>, since: Option<Timestamp>, cursor: Option<Cursor>) -> IoResult<Envelope> {
         let l = limit.map(|v| v.to_string()).unwrap_or("".to_string());
         let s = since.map(|v| v.to_string()).unwrap_or("".to_string());
         let c = cursor.map(|v| v.to_string()).unwrap_or("".to_string());
-        let result = try!(self.get(obj, qs![limit -> l[], modified_after -> s[], cursor -> c[]]));
+        let result = try!(self.get(obj, qs![limit -> l[], modified_after -> s[], cursor -> c[]][]));
         match json::decode::<Envelope>(result.as_slice()) {
             Ok(env) => match env.error {
                 None => Ok(env),
@@ -104,8 +102,9 @@ impl PbAPI {
         }
     }
 
-    pub fn load_by_iden<R: PbObj + JsonDecodable>(&self, obj: &str, iden: Iden) -> IoResult<R> {
-        self.get(format!("{}/{}", obj, iden).as_slice(), qs![]).map(|v| json::decode(v.as_slice()).unwrap())
+    pub fn load_by_iden<R: PbObj>(&self, obj: &str, iden: Iden) -> IoResult<R> 
+        where R: Decodable<json::Decoder, json::DecoderError> {
+        self.get(format!("{}/{}", obj, iden).as_slice(), &[]).map(|v| json::decode(v.as_slice()).unwrap())
     }
 
     pub fn load_since(&self, obj: &str, since: Timestamp) -> IoResult<Envelope> { self._load(obj, None, Some(since), None) }
@@ -119,7 +118,7 @@ impl PbAPI {
 
 #[test]
 fn test_get_objects() {
-    let api = PbAPI::new("XXXXXXX");
+    let api = PbAPI::new(env!("PB_API_KEY"));
     for obj in vec!["pushes", "devices", "contacts", "channels", "clients", "grants", "subscriptions"].iter() {
         let result = api.loadn(*obj, 10);
         match result {
@@ -138,7 +137,7 @@ fn test_get_objects() {
 
 #[test]
 fn test_error() {
-    let api = PbAPI::new("XXXXXXX");
+    let api = PbAPI::new(env!("PB_API_KEY"));
     let result = api.load("invalid_object");
     match result {
         Ok(env) => fail!("expected error, got {}", env),
@@ -148,14 +147,14 @@ fn test_error() {
 
 #[test]
 fn test_delete() {
-    let api = PbAPI::new("XXXXXXX");
+    let api = PbAPI::new(env!("PB_API_KEY"));
     let result = api.delete("pushes/123");
     assert_eq!(result, Ok(()));
 }
 
 #[test]
 fn test_load_by_iden() {
-    let api = PbAPI::new("XXXXXXX");
-    let result = api.load_by_iden("pushes", "123".to_string());
+    let api = PbAPI::new(env!("PB_API_KEY"));
+    let result = api.load_by_iden::<Push>("pushes", "123".to_string());
     fail!("{}", result);
 }

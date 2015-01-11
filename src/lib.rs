9a3f47022710999788aaf155cbe696a227bc2540
Konstantin Stepanov <me@kstep.me>
2015-01-11 13:27:10 +0300

    expose even more structs to root mod

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,9 @@ extern crate hyper;
 extern crate url;
 extern crate "rustc-serialize" as rustc_serialize;
 
-pub use objects::{Iden, Cursor, Timestamp, Envelope, Push, PushData, Account, Device, Contact, Client, Channel, ChannelInfo, Subscription, Grant, ListItem};
+pub use objects::{Iden, Cursor, Timestamp, Envelope, Push, PushData, Account, Device, Contact, Client, Channel, ChannelInfo, Subscription, Grant, ListItem, Error};
 pub use messages::{TargetIden, PushMsg, DeviceMsg, ContactMsg};
+pub use api::{PbAPI, PbError, PbResult, PbVec};
 
 pub mod objects;
 pub mod events;
